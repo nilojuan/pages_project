7545268ec5d7b3f4a9dcf4be4284d562fe27d33b
nilojuan <nilojuan@gmail.com>
2021-01-09 11:35:09 -0500

    add Procfile

---
+++ blog/urls.py
@@ -0,0 +1,16 @@
+# blog/urls.py
+from django.urls import path
+
+#from .views import BlogListView
+from .views import (BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView,) # new # new # new # new
+
+urlpatterns = [
+    path('post/<int:pk>/delete/',
+BlogDeleteView.as_view(), name='post_delete'),
+    path('post/<int:pk>/edit/',
+BlogUpdateView.as_view(), name='post_edit'), # new
+    path('post/new/', BlogCreateView.as_view(), name='post_new'), # new
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'), # new
+    path('', BlogListView.as_view(), name='home'),
+
+]
