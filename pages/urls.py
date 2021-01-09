e80f43c4b05f997d33900f62ffe09d2c4f7e4d2a
nilojuan <nilojuan@gmail.com>
2021-01-09 10:38:50 -0500

    imagenes covid

--- pages/urls.py
+++ pages/urls.py
@@ -2,9 +2,10 @@
 
 from django.urls import path
 
-from .views import HomePageView, AboutPageView # new
+from .views import HomePageView, AboutPageView, CovidPageView # new # new
 
 urlpatterns = [
+path('covid/', CovidPageView.as_view(), name='covid'), # new   
 path('about/', AboutPageView.as_view(), name='about'), # new
 path('', HomePageView.as_view(), name='home'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -6,3 +6,6 @@ class HomePageView(TemplateView):
 
 class AboutPageView(TemplateView):
     template_name = 'about.html'
+
+class CovidPageView(TemplateView):
+    template_name = 'covid.html'
